1e0ae5c37c157e910c97137c322460fcd120d022
HB2FT <131788619+HB2FT@users.noreply.github.com>
2024-02-14 22:10:58 +0300

    Repository Import

---
+++ Mir.Serialization/Mir.Serialization/InputStream.cs
@@ -0,0 +1,44 @@
+﻿//
+// Reading section
+//
+using System;
+using System.IO;
+
+namespace Mir.Serialization
+{
+    public class InputStream
+    {
+        private FileStream fs;
+
+        public InputStream(FileStream fs)
+        {
+            this.fs = fs;
+        }
+
+        public int ReadInt()
+        {
+            int ch1 = fs.ReadByte();
+            int ch2 = fs.ReadByte();
+            int ch3 = fs.ReadByte();
+            int ch4 = fs.ReadByte();
+
+            if ((ch1 | ch2 | ch3 | ch4) < 0) throw new System.Exception("Integer okunamadı.");
+
+            return ((ch1 << 24) + (ch2 << 16) + (ch3 << 8) + (ch4 << 0));
+        }
+
+        public bool ReadBoolean()
+        {
+            int ch = fs.ReadByte();
+
+            if (ch < 0) throw new System.Exception("Boolean okunamadı.");
+
+            return (ch != 0);
+        }
+
+        public float ReadFloat()
+        {
+            return BitConverter.UInt32BitsToSingle((uint)ReadInt());
+        }
+    }
+}
---
+++ Mir.Serialization/Mir.Serialization/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("4000c499-f635-412e-b16c-68f30990a926")]
